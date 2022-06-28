f356b584fcd599d40cb73eb8faca4d64ebfff651
JessM <jess.t.mulliken@gmail.com>
2022-06-28 10:37:18 -0400

    Feat: implemented length and lowercase generator, uppercase is in progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +9,101 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+/**
+ * 1. get criterias
+ * gen chars based on each criteria
+ */
+function generatePassword() {
+  let criteria = [
+    getLength,
+    includeLower,
+    includeUppercase,
+    includeNumeric,
+    includeSpecialChars,
+  ];
+}
+
+function makeRandomChar(index) {
+  let lower = "a".charCodeAt(0); //98
+  let upper = "z".charCodeAt(0); //98+26
+  let numberSpace = upper - lower;
+
+  // 98: a => + (50% * 26)
+  let charIndex = lower + Math.floor(Math.random() * numberSpace);
+  let result = String.fromCharCode(charIndex);
+
+  console.log(index, result);
+
+  return result;
+}
+
+/** @returns { string[] }}  lower case chars*/
+function getLength() {
+  let length = prompt("Specify the length of the password between 8 and 128");
+  if (Number.isNaN(length)) {
+    length = getLength();
+  } else if (length < 8) {
+    length = 8;
+  } else if (length > 128) {
+    length = 128;
+  }
+
+  return new Array(Number(length))
+    .fill(null)
+    .map((_, index) => makeRandomChar());
+}
+
+function includeLower(/** @type string[] */ charArr) {
+  let result;
+  switch (prompt('Should lowercases be included, type "yes" or "no"')) {
+    case "yes": {
+      result = charArr;
+      break;
+    }
+    case "no":
+      {
+        result = charArr.map((/** @type string */ char) =>
+          char.toLocaleUpperCase()
+        );
+      }
+      break;
+    default: {
+      result = includeLower(charArr);
+    }
+  }
+  return result;
+}
+
+function includeUppercase(/** @type String[] */ charArray) {
+  let result;
+  switch(prompt('Should uppercases be included')){
+    case 'yes':{
+      //get all lowercase indexes,
+      // conveert half of them to uppercase
+
+      let lowerCaseIndexes = charArray.map((char,index)=> [char, index]).filter(([char])=> /[a-z]/.test(char) )
+
+      for(cont [lowerCaseChar,index] of lowerCaseIndexes){
+        
+      }
+      break;
+    };
+    case 'no':{
+      result = charArray;
+      break;
+    }
+    default:{
+      result = includeUppercase(charArray);
+    }
+    return result;
+  }
+}
+
+function includeNumeric() {}
+
+function includeSpecialChars() {}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
