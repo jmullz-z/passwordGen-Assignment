5bd2f1e0b2d5b699997b1e32e08ca94228e84f36
JessM <jess.t.mulliken@gmail.com>
2022-08-05 11:30:51 -0400

    created arrays

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,38 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+// Array of numeric characters to be included in password
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,94 +48,90 @@ function writePassword() {
  * gen chars based on each criteria
  */
 function generatePassword() {
-  let criteria = [
-    getLength,
-    includeLower,
-    includeUppercase,
-    includeNumeric,
-    includeSpecialChars,
-  ];
-}
+//   let criteria = [
+//     getLength,
+//     includeLower,
+//     includeUppercase,
+//     includeNumeric,
+//     includeSpecialChars,
+//   ];
+// }
 
-function makeRandomChar(index) {
-  let lower = "a".charCodeAt(0); //98
-  let upper = "z".charCodeAt(0); //98+26
-  let numberSpace = upper - lower;
+// function makeRandomChar(index) {
+//   let lower = "a".charCodeAt(0); //98
+//   let upper = "z".charCodeAt(0); //98+26
+//   let numberSpace = upper - lower;
 
-  // 98: a => + (50% * 26)
-  let charIndex = lower + Math.floor(Math.random() * numberSpace);
-  let result = String.fromCharCode(charIndex);
+//   // 98: a => + (50% * 26)
+//   let charIndex = lower + Math.floor(Math.random() * numberSpace);
+//   let result = String.fromCharCode(charIndex);
 
-  console.log(index, result);
+//   console.log(index, result);
 
-  return result;
-}
+//   return result;
+// }
 
-/** @returns { string[] }}  lower case chars*/
-function getLength() {
-  let length = prompt("Specify the length of the password between 8 and 128");
-  if (Number.isNaN(length)) {
-    length = getLength();
-  } else if (length < 8) {
-    length = 8;
-  } else if (length > 128) {
-    length = 128;
-  }
+// /** @returns { string[] }}  lower case chars*/
+// function getLength() {
+//   let length = prompt("Specify the length of the password between 8 and 128");
+//   if (Number.isNaN(length)) {
+//     length = getLength();
+//   } else if (length < 8) {
+//     length = 8;
+//   } else if (length > 128) {
+//     length = 128;
+//   }
 
-  return new Array(Number(length))
-    .fill(null)
-    .map((_, index) => makeRandomChar());
-}
+//   return new Array(Number(length))
+//     .fill(null)
+//     .map((_, index) => makeRandomChar());
+// }
 
-function includeLower(/** @type string[] */ charArr) {
-  let result;
-  switch (prompt('Should lowercases be included, type "yes" or "no"')) {
-    case "yes": {
-      result = charArr;
-      break;
-    }
-    case "no":
-      {
-        result = charArr.map((/** @type string */ char) =>
-          char.toLocaleUpperCase()
-        );
-      }
-      break;
-    default: {
-      result = includeLower(charArr);
-    }
-  }
-  return result;
-}
+// function includeLower(/** @type string[] */ charArr) {
+//   let result;
+//   switch (prompt('Should lowercases be included, type "yes" or "no"')) {
+//     case "yes": {
+//       result = charArr;
+//       break;
+//     }
+//     case "no":
+//       {
+//         result = charArr.map((/** @type string */ char) =>
+//           char.toLocaleUpperCase()
+//         );
+//       }
+//       break;
+//     default: {
+//       result = includeLower(charArr);
+//     }
+//   }
+//   return result;
+// }
 
-function includeUppercase(/** @type String[] */ charArray) {
-  let result;
-  switch(prompt('Should uppercases be included')){
-    case 'yes':{
-      //get all lowercase indexes,
-      // conveert half of them to uppercase
+// function includeUppercase(/** @type String[] */ charArray) {
+//   let result;
+//   switch(prompt('Should uppercases be included')){
+//     case 'yes':{
+//       //get all lowercase indexes,
+//       // conveert half of them to uppercase
 
-      let lowerCaseIndexes = charArray.map((char,index)=> [char, index]).filter(([char])=> /[a-z]/.test(char) )
+//       let lowerCaseIndexes = charArray.map((char,index)=> [char, index]).filter(([char])=> /[a-z]/.test(char) )
 
-      for(cont [lowerCaseChar,index] of lowerCaseIndexes){
+//       for(cont [lowerCaseChar,index] of lowerCaseIndexes){
         
-      }
-      break;
-    };
-    case 'no':{
-      result = charArray;
-      break;
-    }
-    default:{
-      result = includeUppercase(charArray);
-    }
-    return result;
-  }
+//       }
+//       break;
+//     };
+//     case 'no':{
+//       result = charArray;
+//       break;
+//     }
+//     default:{
+//       result = includeUppercase(charArray);
+//     }
+//     return result;
+//   }
 }
-
-function includeNumeric() {}
-
-function includeSpecialChars() {}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
